2147ae94e6226d14701976d2028e3c28a8f45cf5
Alexander Tsarapkine <alexander.tsarapkine@gmail.com>
2023-06-05 23:22:09 -0400

    added terminal interface, added to gitignore, added dependency

--- README.md
+++ README.md
@@ -9,4 +9,7 @@ Add the keys to the enviornment
 - Create a new .env file (`touch .env`) 
 - Add key to .env `OPENAI_API_KEY="..."`
 
+Run comment single file command:
+`py main.py comment single {filename}`
 
+For more information, use `--help`
--- models/model.py
+++ models/model.py
@@ -1,18 +1,34 @@
 from pydantic import BaseModel
 from utilities.utilities import read_file
 from agents.commentor import generate_comment
+import typer
+
 
 class Terminal(BaseModel):
 
     @staticmethod
     def run():
-        file_name = input("Enter file name: ")
-        file_text = read_file(file_name)
-        print(f"Read File: {file_name}")
+        app = typer.Typer()
+        commentor_app = typer.Typer()
+        # pr_app = typer.Typer()
 
-        print("Generating Comment...")
-        generate_comment(file_text)
-        print("Done!")
+        app.add_typer(commentor_app, name="comment")
+
+        @commentor_app.command("all")
+        def comment_all(root: str):
+            # do stuff to comment everything starting from root
+            pass
+            
+        @commentor_app.command("single")
+        def comment_single(file: str):
+            file_text = read_file(file)
+            print(f"Read File: {file}")
+            print("Generating Comment...")
+            generate_comment(file_text)
+            print("Done!")
+
+        app()
+
 
 
 
